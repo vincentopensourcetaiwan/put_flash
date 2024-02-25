9efbe7561970ac75685deaa9853da2b41fc04203
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2024-02-25 18:20:57 +0800

    setup click event to put flash message

--- lib/put_flash_web/live/home.ex
+++ lib/put_flash_web/live/home.ex
@@ -4,8 +4,18 @@ defmodule PutFlashWeb.Home do
   def render(assigns) do
     ~H"""
     <h1>Welcome to Put Flash!</h1>
-    <.button>info flash</.button>
-    <.button>error flash</.button>
+    <.button phx-click="click_info_flash">info flash</.button>
+    <.button phx-click="click_error_flash">error flash</.button>
     """
+  end
+
+  def handle_event("click_info_flash", _params, socket) do
+    socket = put_flash(socket, :info, "This is an info flash")
+    {:noreply, socket}
+  end
+
+  def handle_event("click_error_flash", _params, socket) do
+    socket = put_flash(socket, :error, "This is an error flash")
+    {:noreply, socket}
   end
 end
